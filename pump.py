89b79c593cad5e06946e1fbbd33d6eec63f861eb
Ryan Cwynar <rtcx86@gmail.com>
2019-12-07 16:00:53 -0500

    remove examples from other project

--- pump.py
+++ pump.py
@@ -1,10 +1,11 @@
 # Test the GPIO control of the relay
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
+
 
 def runPump():
     channel = 4
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(channel, GPIO.OUT)
     GPIO.output(channel, 1)
     print("switch is on")
